8dccb7c4ed82139408fd4835cdab186defed1af7
JamalG <15924489+JamalG94@users.noreply.github.com>
2019-02-26 13:06:40 +0100

    Refractored code to single responsibility

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,7 @@
-import {
-  AfterViewInit,
-  Component,
-  OnInit
-} from '@angular/core';
+import {AfterViewInit, Component, OnChanges, OnInit} from '@angular/core';
 // import plugin
 import '../../node_modules/leaflet-routing-machine/dist/leaflet-routing-machine.js';
-import {
-  LatLong
-} from './latlong.model';
+import {LatLong} from './latlong.model';
 
 declare let L;
 
@@ -19,29 +13,48 @@ declare let L;
 export class AppComponent implements OnInit {
 
   localCoords: LatLong;
+  givenCoords: LatLong;
 
   constructor() {
   }
 
   ngOnInit() {
+    this.getCoordinates();
+  }
+
+  getCoordinates() {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(position => {
 
-        const map = L.map('map').setView([51.505, -0.09], 13);
+        this.localCoords = new LatLong(position.coords.latitude, position.coords.longitude);
+        const map = this.createMap(this.localCoords.lat, this.localCoords.long, 14);
 
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-          attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        }).addTo(map);
+        this.setTileLayer(map);
 
-        L.Routing.control({
-          waypoints: [
-            L.latLng(position.coords.latitude, position.coords.longitude),
-            L.latLng(57.6792, 11.949)
-          ]
-        }).addTo(map);
+        this.setMarkers(map);
+
       });
     } else {
       console.log('Your browser doesnt support this shit.');
     }
+  }
+
+  createMap(lat: number, long: number, zoom: number) {
+    return L.map('map').setView([lat, long], zoom);
+  }
+
+  setTileLayer(map) {
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+  }
+
+  setMarkers(map) {
+    L.Routing.control({
+      waypoints: [
+        L.latLng(this.localCoords.lat, this.localCoords.long),
+        L.latLng(57.6792, 11.949)
+      ]
+    }).addTo(map);
   }
 }
